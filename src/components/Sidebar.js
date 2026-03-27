7165987ad4809f71a5abfd52cf307f187a01f5c6
khadijamuneer <khadijamuneer33@gmail.com>
2026-03-27 23:34:28 +0500

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -45,6 +45,19 @@ const NAV_ITEMS = [
       </svg>
     ),
   },
+
+  {
+    label: "Interview History",
+    path: "/interview-history",
+    icon: (
+      <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+        <path d="M3 3v18h18"/>
+        <path d="M18 17V9"/>
+        <path d="M13 17V5"/>
+        <path d="M8 17v-3"/>
+      </svg>
+    ),
+  },
   {
     label: "Learn Skills",
     path: "/learn",
